6869e48e59c1105c4a6cea3198010f630257148f
Akhmad Salafudin <axmad386@gmail.com>
2021-11-16 09:36:36 +0700

    fix(routing): flatten middleware stack

--- vendor/laranode/Illuminate/Types.ts
+++ vendor/laranode/Illuminate/Types.ts
@@ -12,4 +12,13 @@ export interface Routes {
   uri: string;
   method: Method;
   action: CallBack;
+  middleware: (string | Middleware | null)[];
 }
+
+type Request = any; //TODO: create Request Facade
+export type MiddlewareStack =
+  | null
+  | Middleware
+  | string
+  | (Middleware | string)[];
+export type Middleware = (req: Request, next: (req: Request) => void) => void;
