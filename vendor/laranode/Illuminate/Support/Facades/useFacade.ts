82f406a8ca731b471419c86669aff48bb68c4eff
Akhmad Salafudin <axmad386@gmail.com>
2021-11-16 10:01:44 +0700

    fix(facade): facade now using one unique singleton

--- vendor/laranode/Illuminate/Support/Facades/useFacade.ts
+++ vendor/laranode/Illuminate/Support/Facades/useFacade.ts
@@ -1,4 +1,6 @@
 function useFacade<T>(clazz: any): T {
+  // uniqId of Facade to that will registered to singleton later
+  const abstract = clazz.name + Date.now();
   // credit to loilo for magic method in js
   // https://gist.github.com/loilo/4d385d64e2b8552dcc12a0f5126b6df8
   const classHandler = Object.create(null);
@@ -6,7 +8,7 @@ function useFacade<T>(clazz: any): T {
     if (name in target) {
       return target[name];
     } else {
-      return target.__getStatic.call(receiver, name);
+      return target.__getStatic.call(receiver, name, abstract);
     }
   };
 
