f8177c673d7f9b5e6da67e6fba160c5d7f908f48
Akhmad Salafudin <axmad386@gmail.com>
2021-11-23 15:55:49 +0700

    feat(view): support svelte as view (#4)
    - route now can return view
    - view file is svelte component (typescript support)﻿
    - support inject data from view to svelte props

--- routes/web.ts
+++ routes/web.ts
@@ -1,12 +1,8 @@
-import Validator from "../vendor/laranode/Illuminate/Support/Facades/Validator";
 import Route from "../vendor/laranode/Illuminate/Support/Facades/Route";
 
-Route.get("/", async (req) => {
-  const validator = Validator.make(req.all(), {
-    id: "required",
-  });
-  if (!(await validator.validate())) {
-    return validator.errors;
-  }
-  return "Hello from WEB";
+Route.get("/", async () => {
+  return view("home");
+});
+Route.get("/counter", async (req) => {
+  return view("counter", { count: req.get("count") || 10 });
 });
--- vendor/laranode/global.d.ts
+++ vendor/laranode/global.d.ts
@@ -1,9 +1,15 @@
 import type Repository from "./Illuminate/Config/Repository";
 import type Application from "./Illuminate/Foundation/Application";
+import type Request from "./Illuminate/Http/Request";
 import type Route from "./Illuminate/Routing/Route";
 import type Env from "./Illuminate/Support/Env";
+import type { ObjectOf } from "./Illuminate/Types";
 
 declare global {
+  interface Window {
+    _view: string;
+    _data: ObjectOf<any>;
+  }
   var app: <T extends string | null | any = null>(
     abstract: T | string = null
   ) => T extends null ? Application : T;
@@ -11,4 +17,6 @@ declare global {
   var config: Repository["get"];
   var env: Env["get"];
   var get_current_dir: (importMetaUrl: string) => string;
+  var view: any;
+  var request: () => Request;
 }
