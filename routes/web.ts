0d6d1fa0eb59dca903b87b7aa7466a02d6b8eefa
Akhmad Salafudin <axmad386@gmail.com>
2021-11-26 10:38:10 +0700

    fix(view): svelte component failed hydrate on nested view fix #5

--- routes/web.ts
+++ routes/web.ts
@@ -6,3 +6,9 @@ Route.get("/", async () => {
 Route.get("/counter", async (req) => {
   return view("counter", { count: req.get("count") || 10 });
 });
+
+Route.prefix("/admin").group(() => {
+  Route.get("/counter", async (req) => {
+    return view("admin.counter", { count: req.get("count") || 10 });
+  });
+});
