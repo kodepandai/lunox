066cba19508fa28b725bc583464672f44e5324ee
Akhmad Salafudin <axmad386@gmail.com>
2022-01-10 14:56:46 +0700

    chore: bump vite and typescript version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-this-alias": "off",
+    "@typescript-eslint/no-empty-interface": "off"
   },
   ignorePatterns: ["*.d.ts"],
   globals: {
