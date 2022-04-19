54d2b6d084bcac5565260b3691422894003b6dc2
Akhmad Salafudin <axmad386@gmail.com>
2022-04-19 14:37:31 +0700

    feat(test): added basic test example

--- artisan.ts
+++ artisan.ts
@@ -1,5 +1,4 @@
 import type ConsoleKernel from "./app/Console/Kernel.js";
-import "./autoload.js";
 import app from "./bootstrap/app.js";
 
 const Kernel = app.make<ConsoleKernel>("ConsoleKernel", { app });
--- bootstrap/app.ts
+++ bootstrap/app.ts
@@ -2,8 +2,12 @@ import { Application } from "lunox";
 import path from "path";
 import HttpKernel from "../app/Http/Kernel";
 import ConsoleKernel from "../app/Console/Kernel";
+import "../autoload";
 
-const basePath = path.join(get_current_dir(import.meta.url), "..");
+const basePath = path.join(
+  get_current_dir(import.meta.url),
+  process.env.APP_ENV == "testing" ? "../dist" : ".."
+);
 const app = new Application(basePath);
 
 app.singleton("HttpKernel", HttpKernel);
