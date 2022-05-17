c19824790d6a954ab8f9a8e10593a7f3050c0e91
Akhmad Salafudin <axmad386@gmail.com>
2022-05-17 12:08:23 +0700

    refactor(view): simplify entry-client and entry-server

--- entry-client.ts
+++ entry-client.ts
@@ -1,8 +1,5 @@
 import { makeView } from "lunox/dist/entry-client.js";
 import "uno.css";
 import "@unocss/reset/tailwind.css";
-const View = makeView(
-  import.meta.glob("./app/resources/view/**/*.svelte"),
-  "./app/resources/view"
-);
+const View = makeView(import.meta.glob("/app/resources/view/**/*.svelte"));
 export default View;
--- entry-server.ts
+++ entry-server.ts
@@ -1,11 +1,4 @@
 import { makeRender } from "lunox/dist/entry-server.js";
-export const render =
-  process.env.NODE_ENV == "production"
-    ? makeRender(
-      import.meta.glob("./app/resources/view/**/*.svelte"),
-      "./app/resources/view"
-    )
-    : makeRender(
-      import.meta.glob("../app/resources/view/**/*.svelte"),
-      "../app/resources/view"
-    );
+export const render = makeRender(
+  import.meta.glob("/app/resources/view/**/*.svelte")
+);
