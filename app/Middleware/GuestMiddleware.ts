36bc6942c1ead3f5d18bca36cb26be810cd0824b
Akhmad Salafudin <axmad386@gmail.com>
2022-06-04 11:39:32 +0700

    fix(middleware):  method not called in GuestMiddleware

--- app/Middleware/GuestMiddleware.ts
+++ app/Middleware/GuestMiddleware.ts
@@ -2,7 +2,7 @@ import type { Middleware } from "lunox/dist/Contracts/Http/Middleware";
 
 const GuestMiddleware: Middleware = {
   async handle(req, next) {
-    if (await req.auth().check) {
+    if (await req.auth().check()) {
       return redirect("/");
     }
     return next(req);
