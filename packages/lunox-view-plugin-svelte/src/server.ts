3ff7c2c9296310745c880cde7a8c5421f0d8ab93
Akhmad Salafudin <axmad386@gmail.com>
2023-03-26 15:50:55 +0700

    feat(lunox-view-plugin-svelte): @lunox/view-plugin-svelte for using lunox with svelte as template engine

---
+++ packages/lunox-view-plugin-svelte/src/server.ts
@@ -0,0 +1,17 @@
+import { makeRenderTransform } from "@lunox/view";
+
+/**
+ * transform view with svelte engine
+ */
+const transformView = async (url: string, View: any, props: any) => {
+  return (await View.render(props)) as {
+    html: string;
+    head: string;
+    css: { code: string };
+  };
+};
+
+/**
+ * render method used in vite ssr
+ */
+export const makeRender = makeRenderTransform(transformView);
