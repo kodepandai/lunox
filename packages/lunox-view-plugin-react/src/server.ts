a430a60b49e8fb65a2dd6db545c1a3592dbccf75
Akhmad Salafudin <axmad386@gmail.com>
2023-03-26 15:50:23 +0700

    feat(lunox-view-plugin-react): @lunox/view-plugin-react for using lunox with react as template engine

---
+++ packages/lunox-view-plugin-react/src/server.ts
@@ -0,0 +1,32 @@
+import { makeRenderTransform } from "@lunox/view";
+import { Helmet } from "react-helmet";
+import ReactDomServer from "react-dom/server";
+import * as JsxRuntime from "react/jsx-runtime";
+
+/**
+ * transform view with react engine
+ */
+const transformView = async (url: string, View: any, props: any) => {
+  const html = ReactDomServer.renderToString(
+    (JsxRuntime as any).jsx(View || "", props)
+  ) as string;
+  const { style, title, meta, link, script } = Helmet.renderStatic();
+  return {
+    html,
+    head: `<head>
+        ${title.toString()}
+        ${meta.toString()}
+        ${link.toString()}
+        ${script.toString()}
+        </head>
+        `,
+    css: {
+      code: style.toString(),
+    },
+  };
+};
+
+/**
+ * render method used in vite ssr
+ */
+export const makeRender = makeRenderTransform(transformView);
