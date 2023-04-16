00c9c67f48222a58bfb14445eb87b8ee12593fca
Akhmad Salafudin <axmad386@gmail.com>
2023-04-16 10:56:15 +0700

    feat(lunox-validation): added validator, factory, and FormRequest

--- packages/lunox-core/src/Foundation/Http/index.ts
+++ packages/lunox-core/src/Foundation/Http/index.ts
@@ -1,5 +1,4 @@
 import Cookie from "./Cookie";
-import FormRequest from "./FormRequest";
 import Kernel from "./Kernel";
 import VerifyCsrfToken from "./Middleware/VerifyCsrfToken";
-export { Kernel, VerifyCsrfToken, Cookie, FormRequest };
+export { Kernel, VerifyCsrfToken, Cookie };
--- packages/lunox-validation/src/index.ts
+++ packages/lunox-validation/src/index.ts
@@ -2,5 +2,12 @@ import ValidationException from "./ValidationException";
 import ValidationServiceProvider from "./ValidationServiceProvider";
 import type { Rule } from "./contracts/Validation";
 import Validator from "./facades/Validator";
+import FormRequest from "./FormRequest";
 
-export { ValidationException, ValidationServiceProvider, Rule, Validator };
+export {
+  ValidationException,
+  ValidationServiceProvider,
+  Rule,
+  Validator,
+  FormRequest,
+};
