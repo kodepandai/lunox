fd6fd2d73d600329ec54591f6eaaab160d5e0378
Akhmad Salafudin <axmad386@gmail.com>
2023-08-05 22:45:26 +0700

    wip(lunox-auth): prepare for unit testing

---
+++ packages/lunox-auth/test/Feature/auth.session.test.ts
@@ -0,0 +1,8 @@
+import TestCase from "../TestCase";
+import { describe, test, expect, it } from "vitest";
+
+TestCase.make();
+
+describe("Auth Session Test", () => {
+  //TODO: make auth session test
+});
