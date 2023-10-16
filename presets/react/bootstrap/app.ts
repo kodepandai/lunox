b6842ece25db61f5da9780a6b5b0251655376013
Akhmad Salafudin <axmad386@gmail.com>
2023-10-16 21:58:35 +0700

    chore: update preset script to includes tsx

--- presets/react/bootstrap/app.ts
+++ presets/react/bootstrap/app.ts
@@ -5,7 +5,8 @@ import path from "path";
 import "../autoload";
 
 const basePath = path.join(get_current_dir(import.meta.url), "..");
-const app = new Application(basePath);
+const ext = path.extname(get_current_filename(import.meta.url));
+const app = new Application(basePath, ext);
 
 app.singleton("HttpKernel", HttpKernel);
 app.singleton("ConsoleKernel", ConsoleKernel);
