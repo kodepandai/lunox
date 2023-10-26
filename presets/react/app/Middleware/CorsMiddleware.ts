fdb61110c6d0231851c73ba59e2347f911a79031
Akhmad Salafudin <axmad386@gmail.com>
2023-10-26 22:17:24 +0700

    fix(presets): incorect example of native middleware

--- presets/react/app/Middleware/CorsMiddleware.ts
+++ presets/react/app/Middleware/CorsMiddleware.ts
@@ -2,12 +2,9 @@ import cors from "cors";
 import type { Middleware } from "@lunoxjs/core/contracts";
 
 const CorsMiddleware: Middleware = {
-  async handleNative(req, res, next) {
-    // cors implementation here
-    return cors({
-      credentials: true,
-      origin: "*",
-    })(req, res, next);
-  },
+  handleNative: cors({
+    credentials: true,
+    origin: "*",
+  }),
 };
 export default CorsMiddleware;
