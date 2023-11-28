e87a3036dd172f9df5762750fbe7ea0209cec063
Akhmad Salafudin <axmad386@gmail.com>
2023-11-28 15:27:29 +0700

    chore(lunox-view-plugin-react): change bundler to tsup

--- packages/lunox-view-plugin-react/rollup.config.mjs
+++
@@ -1,6 +0,0 @@
-import { bundleTs } from "@lunoxjs/build";
-export default [
-  ...bundleTs(["src/client.ts", "src/server.ts", "src/vite.ts"], {
-    declaration: process.env.NODE_ENV == "production",
-  }),
-];
