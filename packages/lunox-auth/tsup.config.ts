432ab491eec8b57d1270711b08b5d8d4dbace30e
Akhmad Salafudin <axmad386@gmail.com>
2023-12-04 14:54:31 +0700

    chore(lunox-auth): change experimentalDts to dts for module augmentation

--- packages/lunox-auth/tsup.config.ts
+++ packages/lunox-auth/tsup.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts", "src/contracts/index.ts", "src/providers/*/index.ts"],
   format: "esm",
-  experimentalDts:true,
+  // currently experimentalDts not work with module augmentation
+  // see https://github.com/egoist/tsup/issues/1052
+  // experimentalDts:true 
+  dts:true, // this dts work but very slow
   clean: true,
   target: "es2022",
   minify:true,
