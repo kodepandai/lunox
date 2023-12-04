fe5b651d9d239c91d60fee48e6d745c4903bb60d
Akhmad Salafudin <axmad386@gmail.com>
2023-12-04 14:57:09 +0700

    chore(lunox-session): change experimentalDts to dts for module augmentation

--- packages/lunox-session/tsup.config.ts
+++ packages/lunox-session/tsup.config.ts
@@ -3,7 +3,8 @@ export default defineConfig({
   entry: ["src/index.ts", "src/contracts/index.ts"],
   format: "esm",
   splitting:false,
-  experimentalDts:true,
+  dts: true,
+  // experimentalDts:true, // for now tsup doesn't support module augmentation
   clean: true,
   target: "es2022",
   minify:true,
