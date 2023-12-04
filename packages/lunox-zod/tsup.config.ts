bde6c3eaa8b822dcfe249b5010974e3fb4d84276
Akhmad Salafudin <axmad386@gmail.com>
2023-12-04 15:00:51 +0700

    chore(lunox-zod): change experimentalDts to dts for module augmentation

--- packages/lunox-zod/tsup.config.ts
+++ packages/lunox-zod/tsup.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts", "src/facades/*.ts"],
   format: "esm",
-  experimentalDts: true,
+  // experimentalDts: true,
+  dts: true,
   clean: true,
   target: "es2022",
   minify: true,
